357d88cf6fd9cbc5ae79c7e71adf27cff78825c8
Anushk Jain <anushk2201@gmail.com>
2024-01-20 18:08:31 +0530

    feat: changed the name of Pages folder to pages for deployment ease. feat: added the KonfHub Ticketing form into the TicketPage.jsx (not responsive).

--- src/Pages/TicketPage.jsx
+++ src/Pages/TicketPage.jsx
@@ -1,10 +1,10 @@
-import Cursor from "../components/Cursor"
-import MultiStepForm from "../forms/multiStepForms"
+import Cursor from "../components/Cursor";
+// import MultiStepForm from "../forms/multiStepForms"
 
 const TicketPage = () => {
-  return (
-    <>
-      <Cursor/>
+	return (
+		<div className='flex justify-around items-center space-y-4 flex-col'>
+			{/* <Cursor/>
     <div className="px-24">
         <div className="flex text-2xl my-3 mb-6 justify-between">
       <h1>Enter your Details</h1>
@@ -12,9 +12,28 @@ const TicketPage = () => {
       <h1>Your Ticket</h1>
         </div>
       <MultiStepForm/>
-    </div>
-    </>
-  )
-}
+    </div> */}
 
-export default TicketPage
+			<Cursor />
+
+			<h1 className='font-semibold text-6xl'>
+				{" "}
+				Welcome to{" "}
+				<span className='font-extrabold text-red-600'>TedxJEC</span>
+			</h1>
+			<h4 className='font-semibold text-4xl'> Register for the event </h4>
+
+			<div className="rounded-md border border-white p-2">
+				<iframe
+					src='https://konfhub.com/widget/tedxjec2024?ticketId=12684'
+					id='konfhub-widget'
+					title='Register for TEDxJEC Jabalpur'
+					width='750'
+					height='500'
+				></iframe>
+			</div>
+		</div>
+	);
+};
+
+export default TicketPage;
