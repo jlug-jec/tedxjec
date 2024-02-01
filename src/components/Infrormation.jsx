c1df74c629477a8c93739d8eb591cf5b065df098
Aryan-219 <aryanagr219@gmail.com>
2024-02-01 23:58:41 +0530

    Changed the content of home page

--- src/components/Infrormation.jsx
+++ src/components/Infrormation.jsx
@@ -1,39 +1,64 @@
 const Infrormation = () => {
   return (
     <div className="flex justify-between space-y-8 items-center flex-col">
-        <div className="bg-red-600 flex justify-center items-center h-32 w-full">
-            <h1 className="text-5xl font-extrabold text-white px-8">About Us</h1>
-        </div>
+      <div className="bg-red-600 flex justify-center items-center h-32 w-full">
+        <h1 className="text-5xl font-extrabold text-white px-8">About Us</h1>
+      </div>
 
       <div className="flex justify-between space-y-5 items-center flex-col">
-      <img src="/tedx.png" alt="tedXjec" className="md:h-24 h-14" />
-        <p className="px-24 text-center font-semibold text-2xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat quae, commodi consequuntur illum itaque provident ipsam accusantium magni eaque exercitationem inventore. Dolor eveniet illum delectus maiores magnam tempora. Itaque, corrupti.
-        Pariatur rerum iusto fugiat nemo rem nostrum itaque, fugit nisi, laborum alias, recusandae magnam voluptates dolore. Ad explicabo error molestias tempore sit quia iste. Temporibus delectus laudantium ipsum nisi facilis.
-        Ex dolorum exercitationem necessitatibus praesentium dignissimos. Perferendis iusto quae eos expedita delectus eaque. Voluptates repudiandae illum nostrum reprehenderit deserunt labore ab nemo facilis maiores, sequi ipsa, autem ullam optio eligendi?
-        Quae et doloribus in eaque numquam ab dignissimos illum beatae quidem exercitationem voluptas consequuntur facere non delectus, sunt laborum nemo ullam voluptate suscipit quod perspiciatis maxime? Odio soluta facilis voluptas.
-        Nam nulla aliquam ipsum excepturi consequuntur atque maxime, eos suscipit vitae dolorum, tempora dolor accusantium velit sit et, optio quis cumque repellendus? Asperiores facere, qui dolorem ipsam nulla quos explicabo?</p>
+        <img src="/tedx.png" alt="tedXjec" className="md:h-24 h-14" />
+        <p className="px-24 text-center font-semibold text-2xl">
+          TEDx is a grassroots initiative, created in the spirit of TED's
+          overall mission to research and discover" ideas worth spreading." TEDx
+          brings the spirit of TED to local communities around the globe through
+          TEDx events. These events are organised by passionate individuals who
+          seek to uncover new ideas and to share the latest research in their
+          local areas that spark conversations in their communities. TEDx events
+          include live speakers and recorded TED Talks, and are organised.
+          independently under a free licence granted by TED. More than 3000
+          events are now held annually.
+        </p>
       </div>
 
       <div className="flex justify-between space-y-5 items-center flex-col">
         <img src="/TED-Logo.png" alt="tedXjec" className="md:h-36 h-14" />
-        <p className="px-24 text-center font-semibold text-2xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat quae, commodi consequuntur illum itaque provident ipsam accusantium magni eaque exercitationem inventore. Dolor eveniet illum delectus maiores magnam tempora. Itaque, corrupti.
-        Pariatur rerum iusto fugiat nemo rem nostrum itaque, fugit nisi, laborum alias, recusandae magnam voluptates dolore. Ad explicabo error molestias tempore sit quia iste. Temporibus delectus laudantium ipsum nisi facilis.
-        Ex dolorum exercitationem necessitatibus praesentium dignissimos. Perferendis iusto quae eos expedita delectus eaque. Voluptates repudiandae illum nostrum reprehenderit deserunt labore ab nemo facilis maiores, sequi ipsa, autem ullam optio eligendi?
-        Quae et doloribus in eaque numquam ab dignissimos illum beatae quidem exercitationem voluptas consequuntur facere non delectus, sunt laborum nemo ullam voluptate suscipit quod perspiciatis maxime? Odio soluta facilis voluptas.
-        Nam nulla aliquam ipsum excepturi consequuntur atque maxime, eos suscipit vitae dolorum, tempora dolor accusantium velit sit et, optio quis cumque repellendus? Asperiores facere, qui dolorem ipsam nulla quos explicabo?</p>
+        <p className="px-24 text-center font-semibold text-2xl">
+          TED is a nonprofit organization devoted to Ideas Worth Spreading.
+          Started in 1984 as a conference where Technology, Entertainment and
+          Design converged, and today covers almost all topics from science to
+          business to global issues, in more than 100 languages. The two annual
+          TED Conferences invite the world's leading thinkers and doers to speak
+          for 18 minutes or less. Meanwhile, independently run TEDx events help
+          to share ideas in communities around the world.
+        </p>
       </div>
 
       <div className="flex justify-between space-y-5 items-center flex-col">
-      <img src="/tedXlogo2.png" alt="tedXjec" className="md:h-36 h-14" />
-        
-        <p className="px-24 text-center font-semibold text-2xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat quae, commodi consequuntur illum itaque provident ipsam accusantium magni eaque exercitationem inventore. Dolor eveniet illum delectus maiores magnam tempora. Itaque, corrupti.
-        Pariatur rerum iusto fugiat nemo rem nostrum itaque, fugit nisi, laborum alias, recusandae magnam voluptates dolore. Ad explicabo error molestias tempore sit quia iste. Temporibus delectus laudantium ipsum nisi facilis.
-        Ex dolorum exercitationem necessitatibus praesentium dignissimos. Perferendis iusto quae eos expedita delectus eaque. Voluptates repudiandae illum nostrum reprehenderit deserunt labore ab nemo facilis maiores, sequi ipsa, autem ullam optio eligendi?
-        Quae et doloribus in eaque numquam ab dignissimos illum beatae quidem exercitationem voluptas consequuntur facere non delectus, sunt laborum nemo ullam voluptate suscipit quod perspiciatis maxime? Odio soluta facilis voluptas.
-        Nam nulla aliquam ipsum excepturi consequuntur atque maxime, eos suscipit vitae dolorum, tempora dolor accusantium velit sit et, optio quis cumque repellendus? Asperiores facere, qui dolorem ipsam nulla quos explicabo?</p>
+        <img src="/tedXlogo2.png" alt="tedXjec" className="md:h-36 h-14" />
+
+        <p className="px-24 text-center font-semibold text-2xl">
+        This College belongs to a network of educational institutions administered by the members of the Society of Jesus, a religious order which runs schools, colleges and universities in more than 105 countries in the world. Established in 1880, St Aloysius College prides itself in its history of 140 years. The motto of the college, “Lucet et Ardet”which means“shine to enkindle” has inspired countless students to become men and women for and with others, thus bring light and joy into the lives of people. Inculcating social awareness and enhancing social concern is one of the main thrusts of the college.
+        </p>
+      </div>
+      <div className="flex justify-between space-y-5 items-center flex-col">
+        <img src="/tedXlogo2.png" alt="tedXjec" className="md:h-36 h-14" />
+
+        <p className="px-24 text-center font-semibold text-2xl">
+          The Rolling hills, the scattered coconut, palm trees, freshwater
+          streams, and hard red-clay tiled-roof buildings, pristine beaches,
+          broad roads and calm localities. Mangalore is a beautiful and
+          culturally rich city known for its unique blend of tradition and
+          modernity. Tedxsac is a independently organized TEDx event held at St
+          Aloysius College. It is a platform for spreading ideas, fostering
+          innovation, inspiring the community through engaging talks and
+          presentations. TEDXSAC is a chance to check out from day-to-day
+          routine go on an intellectual journey with smart and curious friends
+          to a world filled with brilliant speakers, captivating performances,
+          amazing new technology, and thought provoking ted talks.
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Infrormation
+export default Infrormation;
