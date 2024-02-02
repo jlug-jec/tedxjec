779f4706e9c16573ac5393e9dd266620f4462a85
Anushk Jain <anushk2201@gmail.com>
2024-02-02 12:29:30 +0530

    feat: all information added except tedXjec.

--- src/components/Infrormation.jsx
+++ src/components/Infrormation.jsx
@@ -1,72 +1,116 @@
 const Infrormation = () => {
-  return (
-    <div className="flex justify-between space-y-14 items-center flex-col">
-      <div className="bg-red-600 flex justify-center items-center h-32 w-full">
-        <h1 className="text-5xl font-extrabold text-white px-8">About Us</h1>
-      </div>
+	return (
+		<div className='flex justify-between space-y-14 items-center flex-col mt-20'>
+			<div className='bg-red-600 flex justify-center items-center h-32 w-full'>
+				<h1 className='text-5xl font-extrabold text-white px-8'>
+					About Us
+				</h1>
+			</div>
 
-      <div className="flex justify-between space-y-12 items-center flex-col">
-        <img src="/tedx.png" alt="tedXjec" className="md:h-24 h-14" />
-        <p className="px-24 text-center font-semibold text-2xl">
-          TEDx is a grassroots initiative, created in the spirit of TED&aapos;s
-          overall mission to research and discover&qout; ideas worth
-          spreading.&quot; TEDx brings the spirit of TED to local communities
-          around the globe through TEDx events. These events are organised by
-          passionate individuals who seek to uncover new ideas and to share the
-          latest research in their local areas that spark conversations in their
-          communities. TEDx events include live speakers and recorded TED Talks,
-          and are organised. independently under a free licence granted by TED.
-          More than 3000 events are now held annually.
-        </p>
-      </div>
+			<div className='flex justify-between space-y-12 items-center flex-col'>
+				<img
+					src='/tedx.png'
+					alt='tedXjec'
+					className='md:h-24 h-10 md:m-0 -m-5'
+				/>
+				<p className='text-center font-semibold md:text-2xl text-lg border border-white md:p-8 p-4 md:mx-8 mx-4'>
+					TEDx is a program of independently organized events that
+					bring people together to share ideas worth spreading. These
+					events are licensed by TED, a nonprofit organization devoted
+					to &quot;Ideas Worth Spreading.&quot; TEDx events are
+					designed to create a TED-like experience at the local level,
+					allowing communities around the world to showcase and
+					discuss ideas that are relevant to them. It&apos;s important
+					to note that while TEDx events are associated with TED, they
+					operate independently, and the content and opinions
+					expressed in TEDx talks are those of the speakers and not
+					necessarily endorsed by TED.
+				</p>
+			</div>
 
-      <div className="flex justify-between space-y-6 items-center flex-col">
-        <img src="/TED-Logo.png" alt="tedXjec" className="md:h-36 h-14" />
-        <p className="px-24 text-center font-semibold text-2xl">
-          TED is a nonprofit organization devoted to Ideas Worth Spreading.
-          Started in 1984 as a conference where Technology, Entertainment and
-          Design converged, and today covers almost all topics from science to
-          business to global issues, in more than 100 languages. The two annual
-          TED Conferences invite the world&apos;s leading thinkers and doers to
-          speak for 18 minutes or less. Meanwhile, independently run TEDx events
-          help to share ideas in communities around the world.
-        </p>
-      </div>
+			<div className='flex justify-between space-y-6 items-center flex-col'>
+				<img
+					src='/TED-Logo.png'
+					alt='tedXjec'
+					className='md:h-36 h-12'
+				/>
+				<p className='text-center font-semibold md:text-2xl text-lg border border-white md:p-8 p-4 md:mx-8 mx-4'>
+					TED is a nonprofit organization devoted to Ideas Worth
+					Spreading. Started in 1984 as a conference where Technology,
+					Entertainment and Design converged, and today covers almost
+					all topics from science to business to global issues, in
+					more than 100 languages. The two annual TED Conferences
+					invite the world&apos;s leading thinkers and doers to speak
+					for 18 minutes or less. Meanwhile, independently run TEDx
+					events help to share ideas in communities around the world.
+					TED has become a global phenomenon, influencing how people
+					consume information and inspiring them to think critically
+					about the world around them. The diverse range of speakers
+					and topics covered in TED Talks has contributed to its
+					widespread popularity and impact.
+				</p>
+			</div>
 
-      <div className="flex justify-between space-y-8 items-center flex-col">
-        <img src="/tedXlogo2.png" alt="tedXjec" className="md:h-36 h-14" />
+			<div className='flex justify-between space-y-8 items-center flex-col'>
+				<img
+					src='/tedXlogo2.png'
+					alt='tedXjec'
+					className='md:h-36 h-16'
+				/>
 
-        <p className="px-24 text-center font-semibold text-2xl">
-          This College belongs to a network of educational institutions
-          administered by the members of the Society of Jesus, a religious order
-          which runs schools, colleges and universities in more than 105
-          countries in the world. Established in 1880, St Aloysius College
-          prides itself in its history of 140 years. The motto of the college,
-          “Lucet et Ardet”which means“shine to enkindle” has inspired countless
-          students to become men and women for and with others, thus bring light
-          and joy into the lives of people. Inculcating social awareness and
-          enhancing social concern is one of the main thrusts of the college.
-        </p>
-      </div>
-      <div className="flex justify-between space-y-10 items-center flex-col">
-        <img src="/tedXlogo2.png" alt="tedXjec" className="md:h-36 h-14" />
+				<p className='text-center font-semibold md:text-2xl text-lg border border-white md:p-8 p-4 md:mx-8 mx-4'>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Repellat explicabo non maiores blanditiis consectetur,
+					voluptatum distinctio provident quo aliquam necessitatibus
+					cupiditate quia natus soluta et veritatis perspiciatis
+					molestias consequatur unde. Facere, voluptates consequuntur
+					sed accusamus culpa vel. Cumque officiis, ex assumenda error
+					recusandae deleniti ipsum tempore aliquid velit enim optio a
+					aut incidunt, maiores accusamus. Quo, ducimus fugiat.
+					Molestias, hic. Quod ipsam autem facilis expedita distinctio
+					vero sequi velit eaque in, natus, quaerat ipsum nam
+					assumenda maxime et mollitia rem inventore repellat
+					quisquam. Veritatis assumenda eaque quos dolorum sapiente?
+					Quae. Harum, quod blanditiis, recusandae ad incidunt dolorum
+					id repellat quas totam provident illo voluptas cupiditate in
+					soluta. Dolores possimus labore neque, distinctio quis
+					inventore placeat soluta eum aliquam deserunt odio. Odio
+					dolor error, et pariatur repudiandae consequatur consectetur
+					recusandae quo porro rem corrupti, similique ea in eos, nisi
+					totam quibusdam fuga ipsum sed facere quas! Officiis,
+					ducimus! Debitis, officiis nisi!
+				</p>
+			</div>
+			<div className='flex justify-between space-y-10 items-center flex-col'>
+				<img src='/jecJAb.png' alt='tedXjec' className='md:h-20 h-10' />
 
-        <p className="px-24 text-center font-semibold text-2xl">
-          The Rolling hills, the scattered coconut, palm trees, freshwater
-          streams, and hard red-clay tiled-roof buildings, pristine beaches,
-          broad roads and calm localities. Mangalore is a beautiful and
-          culturally rich city known for its unique blend of tradition and
-          modernity. Tedxsac is a independently organized TEDx event held at St
-          Aloysius College. It is a platform for spreading ideas, fostering
-          innovation, inspiring the community through engaging talks and
-          presentations. TEDXSAC is a chance to check out from day-to-day
-          routine go on an intellectual journey with smart and curious friends
-          to a world filled with brilliant speakers, captivating performances,
-          amazing new technology, and thought provoking ted talks.
-        </p>
-      </div>
-    </div>
-  );
+				<p className='text-center font-semibold md:text-2xl text-lg border border-white md:p-8 p-4 md:mx-8 mx-4'>
+					Jabalpur Engineering College (JEC) is a well-known
+					engineering institution located in Jabalpur, Madhya Pradesh,
+					India. Jabalpur Engineering College was established in 1947,
+					making it one of the oldest engineering colleges in India.
+					JEC is affiliated with Rani Durgavati Vishwavidyalaya
+					(RDVV), Jabalpur. RDVV is a prominent university in the
+					region. JEC offers undergraduate and postgraduate programs
+					in various engineering disciplines, including Civil
+					Engineering, Mechanical Engineering, Electrical Engineering,
+					Electronics and Telecommunication Engineering, Computer
+					Science and Engineering, and Information Technology. The
+					college may have received accreditation from relevant
+					authorities. Accreditation ensures that the institution
+					meets certain quality standards in its education and
+					infrastructure. JEC likely has a campus equipped with
+					facilities such as classrooms, laboratories, libraries,
+					hostels, sports facilities, and more. The college has a team
+					of experienced faculty members who contribute to the
+					academic and research activities of the institution. JEC is
+					likely involved in organizing various extracurricular
+					activities, cultural events, and technical fests, providing
+					students with a well-rounded educational experience.
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Infrormation;
