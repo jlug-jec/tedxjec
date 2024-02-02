7123eb40faa90554e2818f59d429435dd867b0d3
Anushk Jain <anushk2201@gmail.com>
2024-02-02 22:44:34 +0530

    Ticketing Is Removed For Now. (#11)
    * feat: added navigation bar
    
    * feat: about us  added
    
    * Added links to home page at modal options
    
    * Changed the content of home page
    
    * added gradient in the modal and fixed some bugs
    
    * Adjusted the padding in home page.
    
    * feat: all information added except tedXjec.
    
    * feat: a bit more responsive touch.
    
    * feat: About us added without the tedxjabalpur info.
    
    * feat: Ticketing Removed for now.
    
    * feat: conflict resolved.
    
    ---------
    
    Co-authored-by: Aryan-219 <aryanagr219@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import "./assets/app.css";
 
-import TicketPage from "./pages/TicketPage";
+// import TicketPage from "./pages/TicketPage";
 import SpeakerPage from "./pages/speakerPages/TanjJain";
 import TanjJain from "./pages/speakerPages/TanjJain";
 import SudhirKumar from "./pages/speakerPages/SudhirKumar";
@@ -13,7 +13,7 @@ function App() {
     <>
       <Routes>
         <Route  path="/" element={<Homepage />} ></Route>
-        <Route  path="/ticket" element={<TicketPage />} ></Route>
+        {/* <Route  path="/ticket" element={<TicketPage />} ></Route> */}
         <Route  path="/tanujain" element={<TanjJain />} ></Route>
         <Route  path="/sudhir" element={<SudhirKumar />} ></Route>
         <Route  path="/sampada" element={<Sampada />} ></Route>
